e02be385cc65e351c1040bf749b25d3ba8a786e9
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-16 14:59:16 +0200

    creazione backtick per popolare il dom con i diversi valori

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 /*
 
 
-Descrizione
- Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
 Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
 id del post, numero progressivo da 1 a n
 nome autore,
@@ -12,7 +10,7 @@ testo del post,
 immagine (non tutti i post devono avere una immagine),
 numero di likes.
 Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
-[e qui è già fatto perchè ve lo passo nello zip :innocente:]
+
 Milestone 2 - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
 Milestone 3 - Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
@@ -80,3 +78,54 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+
+const square = document.getElementById('container');
+
+
+
+
+
+
+
+for (let i = 0; i < posts.length; i++){
+
+    let thisPost = posts[i];
+    console.log(thisPost);
+
+    
+
+   square.innerHTML += `<div class="post">
+   <div class="post__header">
+       <div class="post-meta">                    
+           <div class="post-meta__icon">
+               <img class="profile-pic" src="${thisPost.image}" alt="Phil Mangione">                    
+           </div>
+           <div class="post-meta__data">
+               <div class="post-meta__author">${thisPost.name}</div>
+               <div class="post-meta__time">${thisPost.created}</div>
+           </div>                    
+       </div>
+   </div>
+   <div class="post__text">${thisPost.content}</div>
+   <div class="post__image">
+       <img src="${thisPost.media}" alt="">
+   </div>
+   <div class="post__footer">
+       <div class="likes js-likes">
+           <div class="likes__cta">
+               <a class="like-button  js-like-button" href="#" data-postid="1">
+                   <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                   <span class="like-button__label">Mi Piace</span>
+               </a>
+           </div>
+           <div class="likes__counter">
+               Piace a <b id="like-counter-1" class="js-likes-counter">${thisPost.likes}</b> persone
+           </div>
+       </div> 
+   </div>            
+</div> `;
+   
+
+}
